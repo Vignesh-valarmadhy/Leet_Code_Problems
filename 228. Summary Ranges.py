3530056e484c1da5cd800454303f91d37a58d0e0
Vignesh-valarmadhy <vignesh.valarmadhy@gmail.com>
2024-09-07 22:49:38 +0530

    7/9/24

---
+++ 228. Summary Ranges.py
@@ -0,0 +1,40 @@
+# You are given a sorted unique integer array nums.
+
+# A range [a,b] is the set of all integers from a to b (inclusive).
+
+# Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+# Each range [a,b] in the list should be output as:
+
+# "a->b" if a != b
+# "a" if a == b
+ 
+
+# Example 1:
+
+# Input: nums = [0,1,2,4,5,7]
+# Output: ["0->2","4->5","7"]
+# Explanation: The ranges are:
+# [0,2] --> "0->2"
+# [4,5] --> "4->5"
+# [7,7] --> "7"
+# Example 2:
+
+# Input: nums = [0,2,3,4,6,8,9]
+# Output: ["0","2->4","6","8->9"]
+# Explanation: The ranges are:
+# [0,0] --> "0"
+# [2,4] --> "2->4"
+# [6,6] --> "6"
+# [8,9] --> "8->9"
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ranges = [] # [start, end] or [x, y]
+        for n in nums:
+            if ranges and ranges[-1][1] == n-1:
+                ranges[-1][1] = n
+            else:
+                ranges.append([n, n])
+
+        return [f'{x}->{y}' if x != y else f'{x}' for x, y in ranges]
