790fed17b842b1df50a3e7dad2c8089deb514645
Vignesh-valarmadhy <vignesh.valarmadhy@gmail.com>
2024-11-07 21:41:59 +0530

    07/11/24

--- 345. Reverse Vowels of a String.py
+++ 345. Reverse Vowels of a String.py
@@ -29,6 +29,17 @@
 
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowels=[i for i in s if i in "aeiouAEIOU"]
-        result=[i if i not in "aeiouAEIOU" else vowels.pop() for i in s]
-        return "".join(result)
+        s= list(s)
+        vowels = set('aeiouAEIOU')
+        i,j = 0 , len(s)-1
+
+        while (i<j):
+            if(s[i] not in vowels):
+                i += 1
+            elif(s[j] not in vowels):
+                j -= 1
+            else:
+                s[i],s[j] = s[j],s[i]
+                i += 1
+                j -= 1
+        return "".join(s)
