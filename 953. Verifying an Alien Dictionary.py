54ca801b7987a7e402b05acfe2899dc41cbc0517
Vignesh-valarmadhy <vignesh.valarmadhy@gmail.com>
2024-11-28 21:50:10 +0530

    28/11/24

--- 953. Verifying an Alien Dictionary.py
+++ 953. Verifying an Alien Dictionary.py
@@ -32,4 +32,4 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         return words == sorted(words,key=lambda word:[order.index(c) for c in word])
-        
+            
