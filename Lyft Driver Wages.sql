dc8c2fc91e9f6fea7fe0f1eb5f8a8a6b5553aa41
Vignesh-valarmadhy <vignesh.valarmadhy@gmail.com>
2025-10-27 20:26:25 +0530

    27/10/25

---
+++ Lyft Driver Wages.sql
@@ -0,0 +1,6 @@
+-- Find all Lyft drivers who earn either equal to or less than 30k USD or equal to or more than 70k USD.
+-- Output all details related to retrieved records.
+
+select * 
+from lyft_drivers
+WHERE yearly_salary <= 30000 OR yearly_salary >= 70000 ;
