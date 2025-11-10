258a18d9d2a21738fe91dfbfe01042eb04fa7c0e
Vignesh-valarmadhy <vignesh.valarmadhy@gmail.com>
2025-11-10 18:56:26 +0530

    10/11/25

---
+++ Number of violations.sql
@@ -0,0 +1,10 @@
+-- You are given a dataset of health inspections that includes details about violations. Each row represents an inspection, and if an inspection resulted in a violation, the violation_id column will contain a value.
+
+
+-- Count the total number of violations that occurred at 'Roxanne Cafe' for each year, based on the inspection date. Output the year and the corresponding number of violations in ascending order of the year.
+
+select YEAR(inspection_date) AS YEAR , count(*) as inspections
+from sf_restaurant_health_violations
+WHERE business_name = 'Roxanne Cafe'
+GROUP BY year
+ORDER BY year asc;
